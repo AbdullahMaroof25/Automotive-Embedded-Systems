6f1aa41438c8d8b7e9956e48f047f167a60095cf
AbdullahMaroof25 <121130164+AbdullahMaroof25@users.noreply.github.com>
2023-12-20 18:08:51 +0200

    Final Version

---
+++ rcc/inc/rcc_types.h
@@ -0,0 +1,17 @@
+
+#ifndef RCC_TYPES_H_
+#define RCC_TYPES_H_
+
+typedef enum{
+	HSI,
+	HSE,
+	PLL
+}rcc_sysClk_t;
+
+typedef enum {
+	AHB,
+	APB1,
+	APB2
+}rcc_busId_t;
+
+#endif /* RCC_TYPES_H_ */
